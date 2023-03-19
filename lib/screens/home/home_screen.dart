66b8ba2b6b7f7302451285c040a5fdeafcb4db8a
Shreyas Gandhi <shreyasgandhi673@gmail.com>
2023-03-19 20:16:59 +0530

    Changes

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auth_project/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -9,53 +10,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  "Courses",
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      color: Colors.black, fontWeight: FontWeight.w600),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ...courses
-                        .map((course) => Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: CourseCard(course: course),
-                            ))
-                        .toList(),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  "Recent",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(fontWeight: FontWeight.w600),
-                ),
-              ),
-              ...recentCourses.map(
-                (course) => Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                  child: SecondaryCourseCard(course: course),
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        color: backgroundColorLight,
+        child: const Text(
+          "This Is Home Page",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
           ),
+          textAlign: TextAlign.center,
         ),
       ),
     );
@@ -72,41 +38,6 @@ class SecondaryCourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-      decoration: BoxDecoration(
-        color: course.bgColor,
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Text(
-                  course.title,
-                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                ),
-                Text(
-                  "Watch video - 15 mins",
-                  style: TextStyle(color: Colors.white60, fontSize: 16),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 40,
-            child: VerticalDivider(
-              color: Colors.white70,
-            ),
-          ),
-          SizedBox(width: 8),
-          SvgPicture.asset(course.iconSrc)
-        ],
-      ),
-    );
+    return Container();
   }
 }
